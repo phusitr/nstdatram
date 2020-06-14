fea435a668be7147f4a277b50a65d3f4a0518539
Phusit Roongroj <52524554+phusitr@users.noreply.github.com>
2020-06-14 15:33:19 +0700

    Update exam.combo.data.php

--- combobox/exam.combo.data.php
+++ combobox/exam.combo.data.php
@@ -1,4 +1,25 @@
+
 <?php
+/**
+ * Short description for file
+ *
+ * Long description for file (if any)...
+ *
+ * PHP version 7.2.x
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @name       exam.combo.data.php
+ * @category   nstdatram
+ * @author     napatsorn seesuwan <seesuwannapatsorn@gmail.com>
+ * @copyright  2020
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @since      File available since Release 1.0.0
+ */
 
 require_once  dirname(__FILE__) . "/config/config.inc.php";
 require_once  dirname(__FILE__) . "/lib/pg.class.php";
